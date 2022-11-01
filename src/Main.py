536d987999547a43576e9a79e24181db0c7a8858
Maciej Franikowski <mfranikowski@gmail.com>
2022-11-01 19:59:59 +0100

    Read and format datasets

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,6 @@
 from classes.ExpertSystem import ExpertSystem
 
 if __name__ == "__main__":
-    system = ExpertSystem('../data/disease.json')
+    system = ExpertSystem()
+    system.loadSymptoms('../data/symptom_severity.csv')
+    system.loadDiseases('../data/dataset.csv')
--- src/classes/ExpertSystem.py
+++ src/classes/ExpertSystem.py
@@ -1,5 +1,27 @@
+import csv
+from multiprocessing.dummy import Array
+
+
 class ExpertSystem:
-	def __init__(self, givenPath):
-		self.dataPath = givenPath
-		print(self.dataPath)
+	def __init__(self):
+		print('constr')
+
+	def loadSymptoms(self, symptomPath):
+		with open(symptomPath, newline='') as csvfile:
+			reader = csv.DictReader(csvfile)
+			self.symptomArray = []
+			for row in reader:
+				self.symptomArray.append(row['Symptom'])
+
+	def loadDiseases(self, diseasePath):
+		with open(diseasePath, newline='') as csvfile:
+			reader = csv.DictReader(csvfile)
+			self.diseaseDictionary = {}
+			for row in reader:
+				self.diseaseDictionary[row['Disease']] = []
+				for value in row:
+					if(value != 'Disease' and row[value] != ''):
+						self.diseaseDictionary[row['Disease']].append(row[value])
+			
+			
 
